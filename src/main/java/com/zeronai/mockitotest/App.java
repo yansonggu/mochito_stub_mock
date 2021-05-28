7c571b88deab77a78afd33709d2ba0052429ab7b
ygu95at <ygu95at@gmail.com>
2021-05-28 13:44:14 -0400

    first commit with readme.md file

---
+++ src/main/java/com/zeronai/mockitotest/App.java
@@ -0,0 +1,13 @@
+package com.zeronai.mockitotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
